618bcc12d4099b0c0368e62d21d2b795713599ee
Jhonny Gutierrez <jhonnygutierrezcr@gmail.com>
2020-04-06 15:45:42 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita <code>src/App.js</code> y guarda para recargar la página.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprender React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
